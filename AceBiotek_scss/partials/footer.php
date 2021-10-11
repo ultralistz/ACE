29a7f9a780f3dc612341f32928a4988ff25f48b8
ulta <ultralistz@gmail.com>
2021-10-12 03:22:50 +0800

    1.footer編輯完成 2. menu下拉式完成 3.大致index前端完成

--- AceBiotek_scss/partials/footer.php
+++ AceBiotek_scss/partials/footer.php
@@ -1,5 +1,56 @@
-<script src="https://unpkg.com/vue"></script>
-<script src="asset/js/main.js"></script>
+        <!--=============== FOOTER ===============-->
+        <footer class="footer section">
 
-</body>
+            <div class="footer__container container grid">
+
+                <div class="footer__content">
+
+                    <div class="footer__content">
+                        <h3 class="footer__title">聯絡地址</h3>
+                        <ul class="footer__links">
+                            <li><p class="footer__link">新竹縣竹北市生醫二路66號</a></li>
+                            <li><p class="footer__link">(新竹生醫園區)</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="footer__content">
+                    <h3 class="footer__title">寄送方式</h3>
+                    <ul class="footer__links">
+                        <li><p class="footer__link">聯絡電話</p></li>
+                        <li><p class="footer__link">TEL：(886) 03-5500909#3409</p></li>
+                        <li><p class="footer__link">FAX：(886) 03-5506766</p></li>
+                        <li><p class="footer__link">客服免付費專線：0800-550-222</p></li>
+                    </ul>
+                </div>
+
+                <div class="footer__content">
+                    <h3 class="footer__title">聯絡信箱</h3>
+                    <ul class="footer__links">
+                        <li><p class="footer__link">service@acebiotek.com</p></li>
+                    </ul>
+
+                    <br>
+
+                    <h3 class="footer__title">社群媒體</h3>
+                    <ul class="footer__links">
+                        <a href="https://www.facebook.com/Acebiotek" class="footer__social-link"><i class='bx bxl-facebook-circle '></i></a>
+                    </ul>
+                </div>
+            </div>
+
+            <p class="footer__copy">© 2021 ACE Biotek. All rights reserved</p>
+            
+        </footer>
+
+        <!--=============== SCROLL UP ===============-->
+        <a href="#" class="scrollup" id="scroll-up">
+            <i class='bx bx-up-arrow-alt scrollup__icon'></i>
+        </a>
+
+        <script src="https://unpkg.com/vue"></script>
+        <script src="asset/js/main.js"></script>
+        <script src="<?php echo SITEURL; ?>asset/js/main.js"></script>
+        
+    </body>
 </html>
--- AceBiotek_scss/partials/menu.php
+++ AceBiotek_scss/partials/menu.php
@@ -5,41 +5,80 @@
         </div>
 
         <div class="nav__menu" id="nav-menu">
-            <ul class="nav__list">
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#home" class="nav__link active-link">首頁</a>
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#products" class="nav__link">產品</a>
-                </li>
+            <ul class="drop-down-menu">
 
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#technology" class="nav__link">專業技術</a>
-                </li>
+            <li class="menu__center">
+                <a href="#" class="menu__bold">首頁</a>
+            </li>
 
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#news" class="nav__link">新聞中心</a>
-                </li>
+            <li class="menu__center">
+                <a href="#" class="menu__bold">產品</a>
+                <ul>
+                    <li><a href="#">紅外線及可見光AI自動檢篩系統</a></li>
+                    <li><a href="#">O-Ring自動手部消毒機器</a></li>
+                    <li><a href="#">智慧手寫輔助系統</a></li>
+                    <li><a href="#">生理感知器</a></li>
+                    <li><a href="#">智慧資料串流系統</a></li>
+                </ul>
+            </li>
 
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#art" class="nav__link">筑波藝廊</a>
-                </li>
+            <li class="menu__center">
+                <a href="#" class="menu__bold">專業技術</a>
+                <ul>
+                    <li><a href="#">太赫茲技術</a></li>
+                    <li><a href="#">人工智慧</a></li>
+                </ul>
+            </li>
 
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#recruit" class="nav__link">人才招募</a>
-                </li>
+            <li class="menu__center">
+                <a href="#" class="menu__bold">新聞中心</a>
+                <ul>
+                    <li><a href="#">新聞資訊</a></li>
+                    <li><a href="#">活動訊息</a></li>
+                </ul>
+            </li>
 
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#corporate" class="nav__link">企業責任</a>
-                </li>
+            <li class="menu__center">
+                <a href="#" class="menu__bold">筑波藝廊</a>
+                <ul>
+                    <li><a href="#">藝廊展覽</a></li>
+                    <li><a href="#">筑波藝廊報</a></li>
+                    <li><a href="#">藝廊介紹</a></li>
+                    <li><a href="#">下載專區</a></li>
+                </ul>
+            </li>
 
-                <li class="nav__item">
-                    <a href="<?php echo SITEURL ?>#about" class="nav__link">關於筑波</a>
-                </li>
+            <li class="menu__center">
+                <a href="#" class="menu__bold">人才招募</a>
+                <ul>
+                    <li><a href="#">廣納賢才</a></li>
+                    <li><a href="#">筑波福利</a></li>
+                    <li><a href="#">教育訓練</a></li>
+                    <li><a href="#">聯絡人資</a></li>
+                </ul>
+            </li>
+
+            <li class="menu__center">
+                <a href="#" class="menu__bold">企業責任</a>
+                <ul>
+                    <li><a href="#">筑波木笑盃</a></li>
+                    <li><a href="#">樂活園地</a></li>
+                </ul>
+            </li>
+
+            <li class="menu__center">
+                <a href="#" class="menu__bold">關於筑波</a>
+                <ul>
+                    <li><a href="#">公司願景</a></li>
+                    <li><a href="#">專業認證</a></li>
+                    <li><a href="#">聯絡我們</a></li>
+                </ul>
+            </li>
 
                 <div class="button__nav">
                     <button type="button" class="button__language"></button>
                 </div>
-
+                
             </ul>
         </div>
 
